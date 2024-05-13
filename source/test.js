c515ef2139a37c3b951efe481f960b0cf32cf87f
Matt Goldberg <matt.q.goldberg@gmail.com>
2024-05-13 00:10:50 -0700

    Testing jsDoc and prettier

--- source/test.js
+++ source/test.js
@@ -1,10 +1,9 @@
 /**
- * A function written sloppily to test Prettier workflow.
+ * A function written poorly to test Prettier workflow.
  *
  * @returns {number} - Should return 3
  */
-function ugly() {
-  let a = 1;
+function ugly() {let a = 1;
   let b = 2;
   return a + b;
 }
