0cd948aaf09d25dbc5a021b235838212eda42d4e
mattqgoldberg <mattqgoldberg@users.noreply.github.com>
2024-05-13 07:16:10 +0000

    Prettified Code!

--- source/test.js
+++ source/test.js
@@ -3,15 +3,12 @@
  *
  * @returns {number} - Should return 3
  */
-function ugly() {let a = 1;
+function ugly() {
+  let a = 1;
   let b = 3;
-
 
   return a + b;
 }
 ugly();
 
-
-foo(bar)
-
-
+foo(bar);
