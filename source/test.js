e15e85b49de342c677cbe1c2e57faf2da481ef55
Matt Goldberg <matt.q.goldberg@gmail.com>
2024-05-13 00:18:16 -0700

    Testing JSDoc

--- source/test.js
+++ source/test.js
@@ -1,14 +1,11 @@
 /**
- * A function written sloppily to test github actions.
+ * A function written poorly to test github actions.
  *
  * @returns {number} - Should return 3
  */
 function ugly() {
   let a = 1;
   let b = 3;
-
   return a + b;
 }
 ugly();
-
-foo(bar);
