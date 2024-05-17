9e8dc4fdd5e06e99a6032167114c0d4b0e488e5f
Daniel Andrews <andrewsdaniel999@hotmail.com>
2024-05-17 01:14:02 -0700

    added filename enforcement to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,45 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import checkFile from 'eslint-plugin-check-file';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["source/js/*.js"],
+    plugins: {
+      "check-file": checkFile,
+    },
+
+    rules: {
+      // Custom naming conventions
+      "camelcase": ["error", {
+        "properties": "always",
+        ignoreDestructuring: true,
+        ignoreImports: true,
+      }], // Enforce lowerCamelCase for all properties
+      "new-cap": ["error", { "newIsCap": true, "capIsNew": false }], // Class names in UpperCamelCase
+      "id-match": [
+        "error",
+        "^[a-z][a-zA-Z0-9]*$|^[A-Z][A-Za-z0-9]*$|^[A-Z_][A-Z0-9_]*$", // Constant names in CONSTANT_CASE
+        {
+          "onlyDeclarations": true,
+          "properties": true
+        }
+      ],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.js": "KEBAB_CASE",
+        },
+      ],
+    }
+  },
+  {
+    ignores: ["**/*.test.js"]
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
 ];
