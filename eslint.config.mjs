1fab4e20de65a4102e3c8eb1796ff7c351cafbdd
Daniel Andrews <andrewsdaniel999@hotmail.com>
2024-05-17 01:35:37 -0700

    added basic jsdoc linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import checkFile from 'eslint-plugin-check-file';
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default [
   {
     files: ["source/js/*.js"],
     plugins: {
       "check-file": checkFile,
+      "jsdoc": jsdoc,
     },
 
     rules: {
@@ -25,12 +27,37 @@ export default [
           "properties": true
         }
       ],
+      // File naming rules
       "check-file/filename-naming-convention": [
         "error",
         {
           "**/*.js": "KEBAB_CASE",
         },
       ],
+      "jsdoc/check-alignment": 1, // Recommended
+      "jsdoc/check-param-names": 1, // Recommended
+      "jsdoc/check-property-names": 1, // Recommended
+      "jsdoc/check-tag-names": 1, // Recommended
+      "jsdoc/check-values": 1, // Recommended
+      "jsdoc/empty-tags": 1, // Recommended
+      "jsdoc/implements-on-classes": 1, // Recommended
+      "jsdoc/multiline-blocks": 1, // Recommended
+      "jsdoc/no-multi-asterisks": 1, // Recommended
+      "jsdoc/require-jsdoc": 1, // Recommended
+      "jsdoc/require-param": 1, // Recommended
+      "jsdoc/require-param-description": 1, // Recommended
+      "jsdoc/require-param-name": 1, // Recommended
+      "jsdoc/require-param-type": 1, // Recommended
+      "jsdoc/require-property": 1, // Recommended
+      "jsdoc/require-property-description": 1, // Recommended
+      "jsdoc/require-property-name": 1, // Recommended
+      "jsdoc/require-returns": 1, // Recommended
+      "jsdoc/require-returns-check": 1, // Recommended
+      "jsdoc/require-returns-description": 1, // Recommended
+      "jsdoc/require-returns-type": 1, // Recommended
+      "jsdoc/require-yields": 1, // Recommended
+      "jsdoc/require-yields-check": 1, // Recommended
+      "jsdoc/valid-types": 1 // Recommended
     }
   },
   {
