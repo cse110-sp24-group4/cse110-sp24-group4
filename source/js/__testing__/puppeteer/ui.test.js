73d6942d9e7c5e89c56e497b868dcfe7bf8ab2c7
Matt Goldberg <matt.q.goldberg@gmail.com>
2024-05-19 22:26:49 -0700

    Added puppeteer dependency and basic test

---
+++ source/js/__testing__/puppeteer/ui.test.js
@@ -0,0 +1,17 @@
+const { default: puppeteer } = require("puppeteer");
+
+describe("Simple Test", () => {
+
+    beforeAll(async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/source/index.html');
+    });
+
+    it("Check link to projects", async () => {
+        let link = await page.$('a');
+        let text = await (await link.getProperty('innerText')).jsonValue();
+        expect(text).toBe('Launch in Browser');
+        console.log(text);
+    });
+});
