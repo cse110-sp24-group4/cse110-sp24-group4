7deac8400ee806e26db8127447f77619191f0487
Person1234565 <josephkan14@gmail.com>
2024-05-29 23:29:52 -0700

    set up pwa, trying out service worker

---
+++ source/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "devdog";
+
+// Installs the service worker. Feed it some initial URLs to cache
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "./",
+        "./index.html",
+        "./js/project.js",
+        "./js/notes.js",
+        "./js/todo.js",
+        "./css/main.css",
+        "./css/index.css",
+        "./css/project.css",
+        "./css/notes.css",
+        "./css/todo.css",
+        "./pages/project.html",
+        "./pages/notes.html",
+      ]);
+    }),
+  );
+});
+
+// Activates the service worker
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
