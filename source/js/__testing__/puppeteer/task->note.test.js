6ea7ce25175dc3ac2cfbe38b53b71596bd031fca
Kekoa-Pickett <103221420+Kekoa-Pickett@users.noreply.github.com>
2024-06-04 15:07:31 -0700

    Added E2E test case for completed tasks to notes
    Created a new testing file to test and make sure completed todo list tasks were properly stored and accounted for in both the todo list and notes list. Also added a task-list count comparison.

---
+++ source/js/__testing__/puppeteer/task->note.test.js
@@ -0,0 +1,66 @@
+const projectId = "testingProject";
+
+describe("Test for adding completed tasks to notes end-to-end tests", () => {
+  beforeAll(async () => {
+      await page.goto(
+          `http://localhost:9000/pages/notes.html?projectId=${projectId}`,
+      );
+  });
+  test("Testing adding to notes feature", async () => {
+    // setting up test, one completed task one uncomplete, 2 notes
+    await page.type("#new-task-name", "Completed task");
+    await page.click("#add-task-button");
+    await page.click('#task-list li input[type="checkbox"]');
+    await page.type("#new-task-name", "Uncomplete task");
+    await page.click("#add-task-button");
+    const addNoteButton = await page.$("#create-note-button");
+    await addNoteButton.click();
+    await addNoteButton.click();
+    
+    await page.click("#add-to-notes"); // start of test
+    const taskCount = await page.$$eval("#task-list li", (els) => els.length);
+    expect(taskCount).toBe(1);
+
+    const taskText = await page.$eval(
+      "#task-list li span",
+      (el) => el.innerText,
+    );
+    expect(taskText).toBe("Uncomplete task");
+
+
+    const notesGrid = await page.$(".notes-grid");
+    const noteBlocks = await notesGrid.$$(".note-block");
+    
+    expect(noteBlocks.length).toBe(3);
+    for (const block of noteBlocks) {
+      const titleValue =  await (
+        await (await block.$(".note-title")).getProperty("innerText")
+      ).jsonValue();
+      const textValue = await (
+        await (await block.$(".note-content")).getProperty("innerText")
+      ).jsonValue();
+      if (titleValue != "New note"){
+        expect(titleValue).toBe("Completed task");
+        expect(textValue).toBe("Put the contents of your finished task here!");
+      } else {
+        expect(textValue).toBe("Put the contents of your note here!")
+      }
+    }
+    // Making sure changes saved to local storage
+    const curLocalStorage = await page.evaluate((projId) => {
+      return JSON.parse(localStorage.getItem(`${projId}#notes`));
+    }, projectId);
+
+    for (const notes of curLocalStorage) {
+      if (notes.title == "New note"){
+        expect(notes.content).toBe("Put the contents of your note here!");
+      } else {
+        expect(notes.title).toBe("Completed task")
+        expect(notes.content).toBe("Put the contents of your finished task here!");
+      }
+    }
+
+    expect(noteBlocks.length).toBe(2);
+    expect(curLocalStorage.length).toBe(2);
+  });
+})
